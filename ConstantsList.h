9ffc91b4f8d9dd1ede358c62aa86c42241e36580
petrgot2406 <p3trgot@yandex.ru>
2024-11-29 21:36:39 +0300

    update

--- ConstantsList.h
+++ ConstantsList.h
@@ -8,10 +8,10 @@ enum Error_list_t
 {
     LIST_OK        = 0,
     ERROR_ADDRESS  = 1,
-    ERROR_SIZE     = 2,
-    ERROR_CAPACITY = 3,
-    ERROR_ANCHOR   = 4,
-    ERROR_FREE     = 5,
+    ERROR_SIZE     = 1 << 1,
+    ERROR_CAPACITY = 1 << 2,
+    ERROR_ANCHOR   = 1 << 3,
+    ERROR_FREE     = 1 << 4,
 };
 
 #endif
